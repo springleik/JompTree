383904f07f370a46ae38ba77758e5a259a4249df
Mark Williamsen <mark@williamsonic.com>
2026-06-21 08:08:44 +0200

    Hello world commit.

---
+++ JompTree.java
@@ -0,0 +1,13 @@
+/* Java Compiler Test Case Generator
+ * File JompTree.java
+ * M. Williamsen
+ * 21 June 2026
+ */
+
+// Populate a Java class with random code
+
+public class JompTree {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
