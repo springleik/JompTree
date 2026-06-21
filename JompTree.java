ff8e64e27ec052e5d8c41a04639637437f40e287
Mark Williamsen <mark@williamsonic.com>
2026-06-21 10:04:57 +0200

    Started implementing.

--- JompTree.java
+++ JompTree.java
@@ -4,10 +4,51 @@
  * 21 June 2026
  */
 
-// Populate a Java class with random code
+import java.io.FileInputStream;
 
+// Populate a Java class with random code
 public class JompTree {
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
+}
+
+// abstract base class to compose objects into a tree structure
+abstract class node
+{
+    // class members
+    static int theDepth;
+    static int nodeCount;
+    static int maxDepth;
+
+    // instance members
+    abstract void populate ();
+    abstract void express ();
+}
+
+// composite class, may have child nodes
+class brnch extends node
+{
+    // class members
+    FileInputStream brnchFile;
+
+    // instance members
+    String pre;
+    String inter;
+    String post;
+    void populate () {;}
+    void express () {;}
+}
+
+// component class, no child nodes
+class leaf extends node
+{
+    // class members
+    FileInputStream leafFile;
+
+    // instance members
+    String the_str;
+    int my_depth;
+    void populate () {;}
+    void express () {;}
 }
